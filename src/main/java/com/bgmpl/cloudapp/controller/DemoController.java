31d2af6f043908b501db4f3ff22223da1ec0ae57
Sadik Diwan <sadik789diwan@gmail.com>
2026-05-03 15:43:46 +0530

    Add getTwoRecords controller

--- src/main/java/com/bgmpl/cloudapp/controller/DemoController.java
+++ src/main/java/com/bgmpl/cloudapp/controller/DemoController.java
@@ -4,9 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class DemoController {
+
+    private static final Map<Integer, String> dataMap = new HashMap<>();
+
+    static {
+        dataMap.put(1, "AKS deployment Success");
+        dataMap.put(2, "Github Actions Pipeline Active");
+    }
 
     @GetMapping("hello")
     public String hello() {
@@ -16,5 +28,18 @@ public class DemoController {
     @GetMapping("health")
     public String health() {
         return "Application is running";
+    }
+
+    @GetMapping("/records")
+    public List<Map<String, Object>> getTwoRecords() {
+        List<Map<String, Object>> response = new ArrayList<>();
+        dataMap.forEach((id, message) -> {
+            Map<String, Object> record = new HashMap<>();
+            record.put("id", id);
+            record.put("status", message);
+            response.add(record);
+
+        });
+        return response;
     }
 }
